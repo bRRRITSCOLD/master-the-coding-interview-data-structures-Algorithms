a85c18d7e8d21e536e09756b0a93e2148942bcfe
Blaine Richardson <blainerrichardson@gmail.com>
2021-07-31 14:35:47 -0400

    feat: section-6: all lectures for section-6 completed

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const sectionThree = require('./section-3');
 const sectionFour = require('./section-4');
+const sectionSix = require('./section-6');
+
 
 (() => {
   try {
     sectionThree.run();
     sectionFour.run();
+    sectionSix.run();
   
     process.exit(0);
   } catch (err) {
